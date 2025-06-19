e55b51941b627d0c01900231d9b921249d62230f
adeshc63 <73774380+adeshc63@users.noreply.github.com>
2025-06-19 15:52:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,87 +3,66 @@ from os import environ, getenv
 
 id_pattern = re.compile(r'^.\d+$')
 
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
-
 # Bot information
-SESSION = environ.get('SESSION', 'Webavbot')
-API_ID = int(environ.get('API_ID', '25928682'))
-API_HASH = environ.get('API_HASH', '65b01e4ef42f8b3a1d5fd988e5aee5c9')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7590752209:AAF7DSX4jNk2dazjS9OOl3n9mW0y1HQRzC4")
-BOT_USERNAME = environ.get("BOT_USERNAME", 'Uchihashiva_bot') # without @ 
+SESSION = environ.get('SESSION', '')
+API_ID = int(environ.get('API_ID', ''))
+API_HASH = environ.get('API_HASH', '')
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
+BOT_USERNAME = environ.get("BOT_USERNAME", '') # without @
 
 # Admins, Channels & Users
-BIN_CHANNEL = int(environ.get("BIN_CHANNEL", '-1002141063405')) # admin your channel in stream 
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", '-1002141063405')) # admin your channel in users log 
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '821243884').split()] # 3567788, 678899, 5889467
-OWNER_USERNAME = environ.get("OWNER_USERNAME", 'marvaldoom') # without @ 
+BIN_CHANNEL = int(environ.get("BIN_CHANNEL", '')) # admin your channel in stream 
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", '')) # admin your channel in users log 
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+OWNER_USERNAME = environ.get("OWNER_USERNAME", '') # without @
 
 # pics information
-PICS = environ.get('PICS', 'https://envs.sh/_pM.jpg')
+PICS = environ.get('PICS', '')
 
 # channel link information
-CHANNEL = environ.get('CHANNEL', 'https://t.me/marvaldoom')
-SUPPORT = environ.get('SUPPORT', 'https://t.me/marvaldoom')
-
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
+CHANNEL = environ.get('CHANNEL', '')
+SUPPORT = environ.get('SUPPORT', '')
 
 # file limit information
-ENABLE_LIMIT = environ.get("ENABLE_LIMIT", False) # True and False
-RATE_LIMIT_TIMEOUT = int(environ.get("RATE_LIMIT_TIMEOUT", "600"))  # limit time 600 = 10 minutes 
-MAX_FILES = int(environ.get("MAX_FILES", "10"))  # file limit 10 file Olay
-
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
+ENABLE_LIMIT = environ.get("ENABLE_LIMIT", False)
+RATE_LIMIT_TIMEOUT = int(environ.get("RATE_LIMIT_TIMEOUT", "600"))
+MAX_FILES = int(environ.get("MAX_FILES", "10"))
 
 # ban information
 BANNED_CHANNELS = [int(banned_channels) if id_pattern.search(banned_channels) else banned_channels for banned_channels in environ.get('BANNED_CHANNELS', '').split()]   
 BAN_CHNL = [int(ban_chal) if id_pattern.search(ban_chal) else ban_chal for ban_chal in environ.get('BAN_CHNL', '').split()]
-BAN_ALERT = environ.get('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.ᴄᴏɴᴛᴀᴄᴛ [ᴀᴠ ᴄʜᴀᴛ ᴏᴡɴᴇʀ](https://telegram.me/marvaldoom) ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>')
+BAN_ALERT = environ.get('BAN_ALERT' , '')
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Lodu12:Lodu12@doremon.reqbb.mongodb.net/")
-DATABASE_NAME = environ.get('DATABASE_NAME', "FileStream")
+DATABASE_URI = environ.get('DATABASE_URI', '')
+DATABASE_NAME = environ.get('DATABASE_NAME', 'FileStream')
 
-# fsub  information
-AUTH_PICS = environ.get('AUTH_PICS', 'https://envs.sh/AwV.jpg')              
-AUTH_CHANNEL = (environ.get("AUTH_CHANNEL", "-1002141063405"))
+# fsub information
+AUTH_PICS = environ.get('AUTH_PICS', '')
+AUTH_CHANNEL = environ.get("AUTH_CHANNEL", "")
 FSUB = environ.get("FSUB", True)
 
 # port information
 PORT = int(getenv('PORT', '2626'))
 NO_PORT = bool(getenv('NO_PORT', False))
 
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
-
 # time information
-PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))
 SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
 
 # Online Stream and Download
 BIND_ADDRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 WORKERS = int(getenv('WORKERS', '4'))
 MULTI_CLIENT = False
-name = str(environ.get('name', 'avbotz'))
+name = str(environ.get('name', ''))
 APP_NAME = None
+
 if 'DYNO' in environ:
     ON_HEROKU = True
-    APP_NAME = str(getenv('APP_NAME')) #dont need to fill anything here
+    APP_NAME = str(getenv('APP_NAME'))
 else:
     ON_HEROKU = False
+
 FQDN = str(getenv('FQDN', BIND_ADDRESS)) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
-HAS_SSL=bool(getenv('HAS_SSL',False))
-if HAS_SSL:
-    URL = "https://marvaldoom.koyeb.app/".format(FQDN)
-else:
-    URL = "https://marvaldoom.koyeb.app/".format(FQDN, "" if NO_PORT else ":" + str(PORT))
-      
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP    isko mai github pa leak hone sa kasia bacha sakta hu
+HAS_SSL = bool(getenv('HAS_SSL', False))
+URL = str(getenv('URL', ''))
